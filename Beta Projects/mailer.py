946d7f56a7023ca062f25eafb795a9d391567aa0
stubanx <gantavyaoo@gmail.com>
2024-08-07 21:04:57 +0530

    new file:   Beta Projects/mailer.py

---
+++ Beta Projects/mailer.py
@@ -0,0 +1,8 @@
+import tkinter
+import smtplib
+#main screen
+master = tkinter.Tk()
+master.title("Custom Python Email App")
+
+#graphics
+tkinter.Label(master, text="Custom Email App", font=('calibri',15))
